63781b89c39ef02f7384246f15e8de5225f1e4c8
lollo23S <laurenzio06@gmail.com>
2025-02-26 11:12:00 +0100

    Oggetto Conto.java

---
+++ Conto.java
@@ -0,0 +1,41 @@
+//import java.util.Scanner;
+
+class Conto {
+    private double contoBancario;
+    private double portafoglio;
+    
+    public Conto(double contoBancario, double portafoglio) {
+        this.contoBancario = contoBancario;
+        this.portafoglio = portafoglio;
+    }
+
+    public void deposita(double saldo) {
+        if (saldo <= portafoglio) {
+            portafoglio -= saldo;
+            contoBancario += saldo;
+        } else {
+            System.out.println("Saldo del portafoglio non sufficiente");
+        }
+    }
+
+    public void preleva(double saldo) {
+        if (saldo <= contoBancario) {
+            contoBancario -= saldo;
+            portafoglio += saldo;
+        } else {
+            System.out.println("Saldo del conto bancario non sufficiente");
+        }
+    }
+
+    public void aggiornaSaldo(int mesi) {
+        contoBancario += mesi * 100;
+    }
+
+    public double getContoBancario() {
+        return contoBancario;
+    }
+
+    public double getPortafoglio() {
+        return portafoglio;
+    }
+}
